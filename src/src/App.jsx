9885028a7c4d0a2fe646e16191827af0beca044c
Fonsitax <mynameisfonja@gmail.com>
2024-10-23 18:04:08 +0200

    deploying

--- src/src/App.jsx
+++ src/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from './components/Navbar';
+import Navbar from './components/NavBar';
 import ProductListing from './components/ProductListing';
 import './tailwind.css';
 import './global.css';
