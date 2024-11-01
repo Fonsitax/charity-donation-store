a4f57d464b9fc5198ee2b0e2796cc2677f9de726
Fonsitax <mynameisfonja@gmail.com>
2024-11-01 21:30:47 +0100

    import mockdata and pathing in app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,36 @@
-import React from 'react';
-import Navbar from './components/NavBar';
-import ProductListing from './components/ProductListing';
-import './tailwind.css';
-import './global.css';
-import Footer from './components/Footer';
-import Hero from './components/Hero';
-import HeroSlogan from './components/HeroSlogan';
+import React from "react";
+import Navbar from "./components/NavBar";
+import ProductListing from "./components/ProductListing";
+import "./tailwind.css";
+import "./global.css";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import HeroSlogan from "./components/HeroSlogan";
+import mockProducts from "./MockData/mockProducts.json";
 
 function App() {
   return (
     <div className="App">
       {/* Header Section */}
-      <header className="fixed top-0 left-0 w-full z-20 bg-transparent"> {/* Make navbar transparent */}
+      <header className="fixed top-0 left-0 w-full z-20 bg-transparent">
+        {" "}
+        {/* Make navbar transparent */}
         <Navbar />
-      </header>  
+      </header>
       {/* Main App Content */}
-      <main className="p-0"> {/* Remove padding to allow hero to take up full screen */}
+      <main className="p-0">
+        {" "}
+        {/* Remove padding to allow hero to take up full screen */}
         <Hero />
         <HeroSlogan />
-        <div className="p-0"> {/* Add padding around product listing */}
+        <div className="p-0">
+          {" "}
+          {/* Add padding around product listing */}
           <ProductListing />
         </div>
       </main>
       {/* Footer Section */}
-      <Footer />  
+      <Footer />
     </div>
   );
 }
